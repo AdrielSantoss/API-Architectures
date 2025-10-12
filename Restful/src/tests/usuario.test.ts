0cc55e052209bc91d98a856477fa82bc6b5a1b77
Adriel dos Santos <adrieldossantos7@gmail.com>
2025-10-11 21:58:33 -0300

    CHORE(Restful): Rename test and remove supertest.
    - Replace supertest with inject() following fastify's recommendation.

---
+++ Restful/src/tests/usuario.test.ts
@@ -0,0 +1,35 @@
+import { buildServer } from '../index'
+import 'dotenv/config';
+import { UsuariosDto } from '../models/usuarioDto';
+
+describe('GET /', () => {
+  let app: ReturnType<typeof buildServer>
+
+  beforeAll(async () => {
+    app = buildServer()
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should return 200 and hello world', async () => {
+    const limit = 1;
+    const page = 1;
+
+    const response = await app.inject({
+      method: 'GET',
+      url: `/usuarios?page=${page}&limit=${limit}`
+    })
+
+    const data = JSON.parse(response.body) as UsuariosDto;
+    
+    expect(response.statusCode).toBe(200)
+    expect(data.data!.length).toBe(limit)
+    expect(data.meta.limit).toBe(limit)
+    expect(data.meta.page).toBe(page)
+    expect(data.meta.total).toBe(2)
+    expect(data.data![0].nome).toBe('Alice')
+  })
+})
