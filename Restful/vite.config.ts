b0756c5359989ee21869e177b12ad05d0881de84
Adriel dos Santos <adrieldossantos7@gmail.com>
2025-10-25 18:10:08 -0300

    CHORE(Restful): Add default value for process.env.DATABASE_URL_TEST.

--- Restful/vite.config.ts
+++ Restful/vite.config.ts
@@ -4,7 +4,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 if (process.env.VITEST) {
-    process.env.DATABASE_URL = process.env.DATABASE_URL_TEST;
+    process.env.DATABASE_URL =
+        process.env.DATABASE_URL_TEST || 'file:./test.db';
 }
 
 export default defineConfig({
